f864120fa897c1effc5309ecf2c167fb4ecb683e
Aleksandr Kvetko <aleks.kvetko@gmail.com>
2017-04-25 20:09:44 +0300

    init

---
+++ background.js
@@ -0,0 +1,9 @@
+﻿var d=document.createElement('div');
+d.style.width='200px';
+d.style.height='100px';
+d.style.background='white';
+var a=document.createElement('a');
+a.innerHTML="Hdfsdfdsf";
+a.href="http://ya.ru";
+d.appendChild(a);
+document.body.insertBefore(d, document.body.firstChild);
